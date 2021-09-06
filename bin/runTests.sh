26937d09f09a2b5b86966e0a7188d667394cfe20
Fabian Jocks <fabian.jocks@hcom.de>
2021-09-06 08:43:27 +0200

    Increased version number to php 8

--- bin/runTests.sh
+++ bin/runTests.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$(dirname "$(readlink -e "$0")")
 docker run -it --rm --name symfony1 \
   -v "${SCRIPT_DIR}/../:/usr/src/symfony1" \
   -w "/usr/src/symfony1" \
-    php:7.2 data/bin/symfony symfony:test --trace ${@}
+    php:8 data/bin/symfony symfony:test --trace ${@}
