4f54dc8e3ca31e95cfcef596622191a9fb15debd
Fabian Jocks <fabian.jocks@hcom.de>
2021-09-10 09:37:36 +0200

    Added symfony.sh to execute symfony console commands within docker container

--- bin/symfony.sh
+++ bin/symfony.sh
@@ -7,5 +7,5 @@ SCRIPT_DIR=$(dirname "$(readlink -e "$0")")
 echo "Running tests with symfony's lime testing framework"
 docker run -it --rm --name symfony1 \
   -v "${SCRIPT_DIR}/../:/usr/src/symfony1" \
-  -w "/usr/src/symfony1" \
-    php:8-cli data/bin/symfony symfony:test --trace ${@}
+  -w "/usr/src/symfony1/test" \
+    php:8-cli ../data/bin/symfony ${@}
