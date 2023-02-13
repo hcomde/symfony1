55096f2a2d8d17dae5711ca6477bfc8c8ada5adc
Sebastian Szablewski <sebastian.szablewski@hcom.de>
2023-02-13 16:23:40 +0100

    Update test scripts (rename used docker container)

--- bin/runSymfonyTests.sh
+++ bin/runSymfonyTests.sh
@@ -6,13 +6,13 @@ SCRIPT_DIR=$(dirname "$(readlink -e "$0")")
 ./installComposer.sh
 
 echo "[Symfony tests] Check configuration"
-docker run -it --rm --name symfony1 \
+docker run -it --rm --name symfony1_tests \
   -v "${SCRIPT_DIR}/../:/usr/src/symfony1" \
   -w "/usr/src/symfony1" \
     php:8 php data/bin/check_configuration.php
 
 echo "[Symfony tests] Run test suites"
-docker run -it --rm --name symfony1 \
+docker run -it --rm --name symfony1_tests \
   -v "${SCRIPT_DIR}/../:/usr/src/symfony1" \
   -w "/usr/src/symfony1" \
     php:8 data/bin/symfony symfony:test --trace
